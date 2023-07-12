e6249f99ea2610dfc0f67d650ad0ed955daa3220
Robert Lowder <rlowder94@gmail.com>
2023-07-11 19:50:22 -0700

    snapple facts :)

---
+++ projects/snapple_facts.py
@@ -0,0 +1,18 @@
+# 6/28/2023 Robert Lowder
+# Practice Exercise
+# Inspired by this reddit post:
+# https://www.reddit.com/r/theydidthemath/comments/13gsfhx/request_how_many_snapple_facts_duplicates_would_i/
+#
+# Derive interesting information from the fact that there are 1624 unique snapple facts.
+# 
+#   1. How many snapples would you need to buy to get at least one of each?
+#   2. How much money would you spend?
+
+
+SNAPPLE_PRICE = 2.50
+UNIQUE_SNAPPLE_CAPS = 1624
+
+
+
+class Snapple:
+    
