e417eeb5c84c1eb5898960c120342a29ef2016f5
ForeverOptimizing <rlowder94@gmail.com>
2024-05-18 05:57:18 -0700

    cs50

---
+++ cs50/camel.py
@@ -0,0 +1,5 @@
+# Assignment https://cs50.harvard.edu/python/2022/psets/2/camel/
+
+camel = input("Camel Case: ")
+
+
---
+++ cs50/figlet.py
@@ -0,0 +1,6 @@
+#   Assignment: https://cs50.harvard.edu/python/2022/psets/4/figlet/
+#   5/15/2024
+
+
+
+
