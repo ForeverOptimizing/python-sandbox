d5d10d1542b82d2d5c12995bd162ebc3044c80f0
Robert Lowder <134641983+ForeverOptimizing@users.noreply.github.com>
2024-11-18 17:54:05 -0800

    update

--- cs50p/fuel.py
+++ cs50p/fuel.py
@@ -11,25 +11,22 @@ while checking:
     denDeci = den.count(".")
     if numDeci > 0  | denDeci > 0:
         pass
-    if num.isdigit() & den.isdigit():
-        num = int(num)
-        den = int(den)
-        percent = num / den * 100
+    if (percent >= 99) & (percent <= 100):
+        print("F")
+        checking = False
+    elif (percent >= 0) & (percent <= 1):
+        print("E")
+        checking = False
+    elif (percent < 0) | (percent > 100):
+        pass
+    elif num == 0:
+        pass
+    else:
+        percent = int(percent)
+        print(f"{percent}%")
+        checking = False
 
-        if (percent >= 99) & (percent <= 100):
-            print("F")
-            checking = False
-        elif (percent >= 0) & (percent <= 1):
-            print("E")
-            checking = False
-        elif (percent < 0) | (percent > 100):
-            pass
-        elif num == 0:
-            pass
-        else:
-            percent = int(percent)
-            print(f"{percent}%")
-            checking = False
+    
 
 
 
@@ -38,6 +35,54 @@ while checking:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 5/20/2024 Implementation
+# checking = True
+# while checking:
+    
+#     fraction = input("Fraction: ")
+#     num, den = fraction.split("/")
+#     numDeci = num.count(".")
+#     denDeci = den.count(".")
+#     if numDeci > 0  | denDeci > 0:
+#         pass
+#     if num.isdigit() & den.isdigit():
+#         num = int(num)
+#         den = int(den)
+#         percent = num / den * 100
+
+#         if (percent >= 99) & (percent <= 100):
+#             print("F")
+#             checking = False
+#         elif (percent >= 0) & (percent <= 1):
+#             print("E")
+#             checking = False
+#         elif (percent < 0) | (percent > 100):
+#             pass
+#         elif num == 0:
+#             pass
+#         else:
+#             percent = int(percent)
+#             print(f"{percent}%")
+#             checking = False
 
 
 
